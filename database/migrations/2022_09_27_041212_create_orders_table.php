c4be7a65668ee18e272164ba831837840afa820b
haw-ms <ms@webimpact.co.in>
2022-09-28 09:21:43 +0530

    changes

---
+++ database/migrations/2022_09_27_041212_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('period_id');
+            $table->id('user_id');
+            $table->string('type');
+            $table->string('value');
+            $table->int('result')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
